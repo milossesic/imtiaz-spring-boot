8ef2d7bdc78bd9d1229064aeae0c00cea39aac1d
milossesic <milosh.sesic@gmail.com>
2021-03-19 17:45:59 +0100

    init commit

---
+++ project-management/src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.jrp.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository employeeRepo;
+	
+	@GetMapping("/new")
+	public String displayEmployeeForm(Model model) {
+		
+		Employee aEmployee = new Employee();
+		model.addAttribute("employee",aEmployee);
+		
+		return "new-employee";
+	}
+	
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		
+		employeeRepo.save(employee);
+		
+		return "redirect:/employees/new";
+		
+	}
+
+}
