0771705241b4bb94232ea981a9cf8b01ed5a3061
milossesic <milosh.sesic@gmail.com>
2021-05-29 18:17:12 +0200

    migration from h2 to postgresql

--- project-management/src/main/java/com/jrp/pma/controllers/HomeController.java
+++ project-management/src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,9 @@ import com.jrp.pma.entities.Project;
 @RequestMapping("/")
 public class HomeController {
 	
+	//@Value("${version}")
+	//private String version;
+	
 	@Autowired
 	ProjectRepository projectRepo;
 	
@@ -30,6 +34,8 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String displayHome(Model model) throws JsonProcessingException {
+		//display version number
+		//model.addAttribute("versionNumber", version);
 		
 		Map<String, Object> map = new HashMap<>();
 		
---
+++ project-management/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,18 @@
+package snippet;
+
+public class Snippet {
+	spring.datasource.url=jdbc:postgresql://localhost:5432/pma-springbootdb
+	spring.datasource.username=postgres
+	spring.datasource.password=password321
+	
+	spring.datasource.initialization-mode=never
+	
+	spring.jpa.hibernate.ddl-auto=none
+	
+	spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation=true
+	
+	spring.jpa.show-sql=true
+	spring.thymeleaf.cache=false
+	version=dev
+}
+
